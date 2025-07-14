1633686246b5d3da9a63b86effbab3c24e4e86d7
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-07-14 17:37:00 -0600

    feat: table modified

--- database/migrations/2025_07_12_193528_create_doctors_table.php
+++ database/migrations/2025_07_12_193528_create_doctors_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Classes\Specialty;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,8 +14,8 @@ return new class extends Migration {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('specialty')->nullable();
             $table->date('birthday');
+            $table->enum('specialty', Specialty::all())->nullable();
             $table->timestamps();
         });
     }
