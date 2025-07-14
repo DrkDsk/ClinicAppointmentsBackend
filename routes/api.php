205fb82ce1524566bab6dff1fcdecfead7229462
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-07-14 15:53:25 -0600

    feat: create role use case

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,21 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', [UserController::class, 'get'])->middleware('auth:sanctum');
-
-Route::prefix('user/admin')
-->name('user.admin')
+Route::prefix('users/admin')
 ->middleware(['auth:sanctum', 'role:admin'])->group(function () {
-    Route::get('get', function () {
-        return "only admins";
-    });
+    Route::get('get', [UserController::class, 'get']);
+    Route::post('create', [UserController::class, 'create']);
 });
 
 Route::prefix('auth')->group(function () {
     Route::post('login', [LoginController::class, 'login']);
+});
+
+Route::prefix('users/roles')
+->middleware('auth:sanctum')->group(function () {
+    Route::post('set', [RoleController::class, 'set']);
 });
