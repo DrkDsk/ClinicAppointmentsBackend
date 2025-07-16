6cdebcd3437cb23e1f20430e4ee1201fcd1b1fd1
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-07-15 23:55:40 -0600

    feat: validation removed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
     {
     }
 
-    public function get(Request $request)
+    public function get(Request $request): UserResource
     {
         $user = $request->user();
 
