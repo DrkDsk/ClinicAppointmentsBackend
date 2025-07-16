0f73255ae45967729a34f5b99daa282d4bd9fe9d
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-07-15 23:55:19 -0600

    feat: get only admins

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,15 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Classes\Role;
 use App\Http\Requests\CreateRoleRequest;
 use App\Http\Resources\UserResource;
+use App\Models\User;
 
 class RoleController extends Controller
 {
-    public function set(CreateRoleRequest $request) : UserResource {
+    public function set(CreateRoleRequest $request): UserResource
+    {
         $user = $request->user();
         $user->syncRoles($request->get('roles'));
 
         return new UserResource($user);
+    }
+
+    public function admins()
+    {
+        $users = User::role(Role::ADMIN)->paginate(20);
+
+        return $users;
     }
 }
