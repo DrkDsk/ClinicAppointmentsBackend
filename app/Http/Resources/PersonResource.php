d8ce5b64be45f9ce83d84869001ba71c896db7d4
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-07-15 23:55:58 -0600

    feat: resources modified

--- app/Http/Resources/PersonResource.php
+++ app/Http/Resources/PersonResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserResource extends JsonResource
+class PersonResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -15,9 +15,11 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'person' => new PersonResource($this->person),
-            'roles' => RoleResource::collection($this->roles)
+            "id" => $this->id,
+            "name" => $this->name,
+            "email" => $this->email,
+            "birthday" => $this->birthday,
+            "phone" => $this->phone,
         ];
     }
 }
