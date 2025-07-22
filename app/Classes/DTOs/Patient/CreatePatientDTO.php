2cae2fe8a95ce2eaa581e5014e297a1543001ffb
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-07-22 15:16:25 -0600

    feat: updated dto to create patient

--- app/Classes/DTOs/Patient/CreatePatientDTO.php
+++ app/Classes/DTOs/Patient/CreatePatientDTO.php
@@ -2,21 +2,41 @@
 
 namespace App\Classes\DTOs\Patient;
 
+use App\Classes\DTOs\Person\PersonDTO;
+use App\Classes\DTOs\User\CreateUserDTO;
 use App\Classes\Enum\HeightMeasureEnum;
 use App\Classes\Enum\WeightMeasureEnum;
-use Carbon\Carbon;
 
 class CreatePatientDTO
 {
     public function __construct(
-        public readonly ?int $userId = null,
-        public readonly ?string $userEmail = null,
-        public readonly Carbon $birthday,
-        public readonly string $phoneNumber,
+        public readonly PersonDTO $person,
         public readonly float $weight,
         public readonly float $height,
         public readonly WeightMeasureEnum $weightMeasureEnum,
-        public readonly HeightMeasureEnum $heightMeasureEnum
+        public readonly HeightMeasureEnum $heightMeasureEnum,
+        public readonly CreateUserDTO $user,
+        public readonly ?int $id = null,
     ) {
+    }
+
+    public function copyWith(
+        ?PersonDTO $person = null,
+        ?float $weight = null,
+        ?float $height = null,
+        ?WeightMeasureEnum $weightMeasureEnum = null,
+        ?HeightMeasureEnum $heightMeasureEnum = null,
+        ?CreateUserDTO $user = null,
+        ?int $id = null
+    ): self {
+        return new self(
+            person: $person ?? $this->person,
+            weight: $weight ?? $this->weight,
+            height: $height ?? $this->height,
+            weightMeasureEnum: $weightMeasureEnum ?? $this->weightMeasureEnum,
+            heightMeasureEnum: $heightMeasureEnum ?? $this->heightMeasureEnum,
+            user: $user ?? $this->user,
+            id: $id ?? $this->id
+        );
     }
 }
