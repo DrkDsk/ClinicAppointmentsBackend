e8f8289aaba16ab94ab5cd6402d74373d079dfa6
Alfredo Palacios <joseaph.1998@gmail.com>
2025-10-22 17:25:35 -0600

    feat: service to search people

--- app/Domain/Services/PersonServiceInterface.php
+++ app/Domain/Services/PersonServiceInterface.php
@@ -5,9 +5,12 @@ namespace App\Domain\Services;
 use App\Classes\DTOs\Person\PersonDTO;
 use App\Models\Person;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Collection;
 
 interface PersonServiceInterface
 {
     public function getAllPaginate(int $perPage = 10) : LengthAwarePaginator;
     public function store(PersonDTO $personDTO): Person;
+
+    public function search(string $query) : Collection;
 }
